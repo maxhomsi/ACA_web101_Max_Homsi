8a09dadf7436e9ae31a7c0ff20e506a639104a71
Ximilis <92741358+maxhomsi@users.noreply.github.com>
2022-01-23 08:10:14 -0600

    noform2

--- form2/scripts.js
+++
@@ -1,6 +0,0 @@
-const placeForResults = document.getElementById("results")
-
-new URLSearchParams(window.location.search).forEach((value, name) => {
-  placeForResults.append(`${name} : ${value}`)
-  placeForResults.append(document.createElement("br"))
-})
